a04365fc0a946a17bec0383fc5999580c71e9d4f
Amelorate <ameilorate2@gmail.com>
2016-02-29 20:58:26 -0500

    Adds a unit test for shutting down an event loop
    I really dread multi-threaded unit tests. Atleast I have rust to tell me what's wrong with my code. It just needs a better way to see if a thread has stopped.
    
    Signed-off-by: Amelorate <ameilorate2@gmail.com>

--- src/net/test.rs
+++ src/net/test.rs
@@ -1,6 +1,5 @@
 use std::thread;
 use std::sync::mpsc::{TryRecvError, channel};
-use std::sync::Arc;
 use std::time::Duration;
 use std::io;
 
@@ -14,7 +13,13 @@ fn get_packet_length_correct() {
     LittleEndian::write_u32(&mut m_number, super::NET_MAGIC_NUMBER);
     let mut intended_length: [u8; 2] = [0; 2];
     LittleEndian::write_u16(&mut intended_length, 10);
-    let length = super::get_packet_length([m_number[0], m_number[1], m_number[2], m_number[3], intended_length[0], intended_length[1]]).expect("get_packet_length wrongly checks NET_MAGIC_NUMBER");
+    let length = super::get_packet_length([m_number[0],
+                                           m_number[1],
+                                           m_number[2],
+                                           m_number[3],
+                                           intended_length[0],
+                                           intended_length[1]])
+                     .expect("get_packet_length wrongly checks NET_MAGIC_NUMBER");
     assert_eq!(length, 10);
 }
 
@@ -32,8 +37,25 @@ fn handler_new() {
 #[test]
 fn event_loop_impl_new() {
     super::EventLoopImpl::new(super::MAX_CONNECTIONS).unwrap();
-    unimplemented!()
 }
 
 #[test]
-fn event_loop_impl_shutdown() {}
+fn event_loop_impl_shutdown() {
+    let (tx, rx) = channel::<Result<(), io::Error>>();
+    let mut event_loop = super::EventLoopImpl::new(super::MAX_CONNECTIONS).unwrap();
+    let event_loop_ref: super::EventLoopImplRef = (&mut event_loop).into();
+    let thread = thread::spawn(move || tx.send(event_loop.run()).unwrap());
+    event_loop_ref.shutdown();
+    thread::sleep(Duration::from_millis(250));
+    // This may, once in a very long time, fail. It really shouldn't, but it is possible.
+    // Just raise the number or try again if it fails.
+    match rx.try_recv() {
+        Err(TryRecvError::Empty) => panic!("EventLoopImpl did not stop after calling shutdown()"),
+        Err(TryRecvError::Disconnected) => {
+            panic!("EventLoop somehow disconnected it's channel without stopping")
+        }
+        Ok(res) => res,
+    }
+    .unwrap();
+    thread.join().unwrap();
+}
