ea549a9c401478513f63a9dc0ab1bb44cdfb30cc
Amelorate <ameilorate2@gmail.com>
2016-03-30 16:03:17 -0400

    Remove forgotten debug function

--- src/script/prelude.lua
+++ src/script/prelude.lua
@@ -1,22 +1,5 @@
 prelude_buildengine = {}
 
--- Print contents of `tbl`, with indentation.
--- `indent` sets the initial level of indentation.
-function tprint (tbl, indent)
-  if not indent then indent = 0 end
-  for k, v in pairs(tbl) do
-    formatting = string.rep("  ", indent) .. k .. ": "
-    if type(v) == "table" then
-      print(formatting)
-      tprint(v, indent+1)
-    elseif type(v) == 'boolean' then
-      print(formatting .. tostring(v))
-    else
-      print(formatting .. v)
-    end
-  end
-end
-
 function prelude_buildengine.package_searcher (modname)
     -- Assumes that prelude_buildengine.modules has the source code of all the modules that can be imported.
     -- That table is added in a step of initing the interpreter.
