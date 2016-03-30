ad0cc7c43cf8cffe27061790a2375e561d5f3d92
Amelorate <ameilorate2@gmail.com>
2016-03-30 18:37:16 -0400

    Adds a test for script::Engine::call_prelude_fn when there are 0 arguments and no returns
    It is however broken by tomaka/hlua#66.

--- src/script/test/mod.rs
+++ src/script/test/mod.rs
@@ -1,12 +1,16 @@
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::collections::HashMap;
 
 use hlua::any::AnyLuaValue;
+use hlua::{LuaTable, function0};
 
 use super::*;
 
 const EVENT: &'static str = include_str!("event.lua");
 const TEST: &'static str = include_str!("test.lua");
 const REQUIRE: &'static str = include_str!("require.lua");
+
+static CALL_FN_NO_ARGS_TEST_VAL: AtomicBool = AtomicBool::new(false);
 
 /// Call Engine.new without any code.
 #[test]
@@ -32,4 +36,25 @@ fn lua_event() {
     let _ = engine.exec_event("test".to_owned(), Vec::new()).expect("Failed to exec event");
     let test_val: AnyLuaValue = engine.interpreter.get("test_val").unwrap();
     assert_eq!(test_val, AnyLuaValue::LuaBoolean(true));
+}
+
+/// Tests calling a prelude function using Engine::call_prelude_fn.
+///
+/// Curently broken until tomaka/hlua#66
+#[test]
+fn call_fn_no_args() {
+    let mut engine = Engine::new(HashMap::new());
+    let fun = function0(|| {
+        CALL_FN_NO_ARGS_TEST_VAL.store(true, Ordering::Relaxed);
+    });
+    {
+        let mut prelude_table: LuaTable<_> = engine.interpreter
+                                                   .get("prelude_buildengine")
+                                                   .expect("Failed to get prelude table.");
+        prelude_table.set("test_fn", fun);
+    }
+    let result = engine.call_prelude_fn("test_fn", Vec::new()).expect("Failed to call test_fn");
+    assert!(result.is_none(),
+            "Engine::call_prelude_fn returned a Some value for a function returning nil: {:?}",
+            result);
 }
